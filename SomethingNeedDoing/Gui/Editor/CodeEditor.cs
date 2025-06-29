2bef818e0afb84c5578d923a640ae99313727218
OhKannaDuh <Liammcnair@outlook.com>
2025-06-29 06:37:04 +0100

    Revert "Ensure Mono font"
    This reverts commit e953fef72f70f1aa0a579f40b84331c618fde2da.

--- SomethingNeedDoing/Gui/Editor/CodeEditor.cs
+++ SomethingNeedDoing/Gui/Editor/CodeEditor.cs
@@ -1,6 +1,4 @@
-﻿using Dalamud.Interface;
-using Dalamud.Interface.Utility.Raii;
-using DalamudCodeEditor;
+﻿using DalamudCodeEditor;
 using TextEditor = DalamudCodeEditor.TextEditor.Editor;
 using SomethingNeedDoing.Core.Interfaces;
 
@@ -64,7 +62,6 @@ public class CodeEditor
         if (macro == null)
             return false;
 
-        using var font = ImRaii.PushFont(UiBuilder.MonoFont);
         _editor.Draw(macro.Name);
         return _editor.Buffer.IsDirty;
     }
